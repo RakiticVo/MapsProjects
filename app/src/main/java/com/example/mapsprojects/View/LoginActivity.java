2fefd4a0deda4c6b19e231aedceba735ac493d63
= <=>
2021-07-06 20:36:25 +0700

    Create Service to get Location Background

--- app/src/main/java/com/example/mapsprojects/View/LoginActivity.java
+++ app/src/main/java/com/example/mapsprojects/View/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.mapsprojects.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,10 @@ import com.example.mapsprojects.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // to get location permissions.
+    private final static int LOCATION_REQUEST_CODE = 23;
+    boolean locationPermission = false;
+
     EditText edt_username, edt_password;
     CheckBox cb_remember;
     SharedPreferences sharedPreferences;
@@ -24,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        requestPermision();
         declareView();
         sharedPreferences = getSharedPreferences("dataLogin", MODE_PRIVATE);
 
@@ -34,6 +43,39 @@ public class LoginActivity extends AppCompatActivity {
         edt_username.setText(sharedPreferences.getString("username", ""));
         edt_password.setText(sharedPreferences.getString("password", ""));
         cb_remember.setChecked(sharedPreferences.getBoolean("checked", false));
+    }
+
+    private void requestPermision() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+        } else {
+            locationPermission = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //if permission granted.
+                    locationPermission = true;
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 
     void declareView(){
